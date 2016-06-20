3fb906b44a86cfdc2dbb66479eb704b7b9b17f4f
edgardo.agno@gmail.com <edgardoagno@EDGARDOs-MacBook-Air.local>
2016-06-20 15:46:07 +0100

    Round robin stub

---
+++ app/src/main/java/com/edgardoagno/tournamentscheduler/Scheduler.java
@@ -0,0 +1,12 @@
+package com.edgardoagno.tournamentscheduler;
+
+/**
+ * Created by edgardoagno on 20/06/16.
+ */
+public class Scheduler {
+
+    public static <Team> void roundRobin(Int round, Team[] teams) {
+
+        System.out.println( "Teams:" );
+    }
+}
